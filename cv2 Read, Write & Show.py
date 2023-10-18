a99a70c18fb1618ba78db684822d64b02ffa7562
Anbalan <140716526+Anbalan777@users.noreply.github.com>
2023-10-18 10:15:30 -0700

    Update cv2 Read, Write & Show.py

--- cv2 Read, Write & Show.py
+++ cv2 Read, Write & Show.py
@@ -3,5 +3,5 @@ img=cv2.imread("sample2.jpg") #ready image from directory
 cv2.imshow("Python",img) #display the image 
 cv2.imwrite("Pytohn.jpg",img) #Save the image
 cv2.waitKey() # time 
-cv2.destroyAllWindows() colse the window an particular time
+cv2.destroyAllWindows() #colse the window an particular time
 
