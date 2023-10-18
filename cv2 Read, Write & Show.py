4ff9a9a05f24788152707f2fecbbbd367fa899c1
Anbalan <140716526+Anbalan777@users.noreply.github.com>
2023-10-18 10:15:02 -0700

    Update cv2 Read, Write & Show.py

--- cv2 Read, Write & Show.py
+++ cv2 Read, Write & Show.py
@@ -1,1 +1,7 @@
+import cv2 #import the opencv lib
+img=cv2.imread("sample2.jpg") #ready image from directory 
+cv2.imshow("Python",img) #display the image 
+cv2.imwrite("Pytohn.jpg",img) #Save the image
+cv2.waitKey() # time 
+cv2.destroyAllWindows() colse the window an particular time
 
