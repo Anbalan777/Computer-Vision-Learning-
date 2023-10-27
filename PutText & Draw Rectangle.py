e5564c0c685db43ef37c88553a77031e56dba7b1
Anbalan <140716526+Anbalan777@users.noreply.github.com>
2023-10-27 06:48:43 -0700

    Update Put Text & Draw Rectangle.py

--- PutText & Draw Rectangle.py
+++ PutText & Draw Rectangle.py
@@ -1,1 +1,5 @@
-
+import cv2 
+img=cv2.imread("sample2.jpg") #Read Image From Directory 
+rect=cv2.rectangle(img,(10,10),(220,220),(2,255,20),5) #Draw a rectangle snytax is cv2.rectangle(src,(x,y),(x+w,x+h),(color),thickness)
+cv2.putText(rect,"Python",(25,25),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2) #cv2.putText(src, text, position,font,fontSize,color,thickness)
+cv2.imshow("Image",img)
